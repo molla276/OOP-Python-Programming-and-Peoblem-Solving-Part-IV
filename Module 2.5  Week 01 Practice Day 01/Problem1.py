dc95dd2915288f6b9be16763c09c7711d89d76f4
ARIFUL ISLAM ARIF <ariful.mr.islam@gmail.com>
2022-10-04 22:28:07 +0600

    Update Problem1.py

--- Module 2.5  Week 01 Practice Day 01/Problem1.py
+++ Module 2.5  Week 01 Practice Day 01/Problem1.py
@@ -9,3 +9,19 @@ num = float((input("Enter a float number: ")))
 print("Floor is",math.floor(num))
 print("Ceil is",math.ceil(num))
 
+# In this program, three different variables a are defined in separate namespaces and accessed accordingly.
+
+def outer_function():
+  global a
+  a = 30
+  
+  def inner_function():
+    global a
+    a = 20
+    print('a=',a)
+  inner_function():
+    print('a=',a)
+
+a = 10
+outer_function()
+print('a=',a)

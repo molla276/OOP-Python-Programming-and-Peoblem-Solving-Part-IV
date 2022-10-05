bbc24ee722ecc9c28a9633eade84d853d177e6ab
ARIFUL ISLAM ARIF <ariful.mr.islam@gmail.com>
2022-10-05 18:33:59 +0600

    OOP and Python

--- Module 2 Conditionals and Loop/for...else and break.py
+++ Module 2 Conditionals and Loop/for...else and break.py
@@ -1,1 +1,11 @@
+""" Program to display student's marks from record"""
+student_name = 'Arif'
 
+marks = {'James': 90, 'Jules' : 95, 'Arif': 100}
+
+for student in marks:
+  if student == student_name:
+    print(marks[student])
+    break
+else:
+  print('No entry with that name found.')

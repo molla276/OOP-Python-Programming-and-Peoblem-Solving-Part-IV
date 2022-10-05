e06eaf000149010a78bfdd38b43dcac1d6dd1fe9
ARIFUL ISLAM ARIF <ariful.mr.islam@gmail.com>
2022-10-05 19:03:12 +0600

    Update while.py

--- Module 2 Conditionals and Loop/while.py
+++ Module 2 Conditionals and Loop/while.py
@@ -6,5 +6,22 @@ while num <= 38 :
     sum = sum + num
 print(sum)
 
+# Program to add natural
+# numbers up to
+# sum = 1 + 2 + 3 +.....+ n
 
+# To take input from the user
+# n = int(input("Enter n: "))
+
+n = 10
+
+# initialize sum and counter
+sum = 0
+i = 1
+
+while i<=n:
+    sum = sum + i #update counter
+    
+# print the sum
+print("The sum is", sum)
 

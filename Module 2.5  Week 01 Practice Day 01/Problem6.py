acb3f7eca8bb53c494b07c9cfc4dd54b8995c21f
ARIFUL ISLAM ARIF <ariful.mr.islam@gmail.com>
2022-10-05 18:16:04 +0600

    Update Problem6.py

--- Module 2.5  Week 01 Practice Day 01/Problem6.py
+++ Module 2.5  Week 01 Practice Day 01/Problem6.py
@@ -1,3 +1,10 @@
+""" Program to iterate through a list using indexing """
+genre = ['Pop', 'Rock', 'Jazz']
+
+# iterate over the list using indexing
+for i in range(len(genre)):
+    print('I love',genre)
+    
 # rows = 6
 # # if you want user to enter a number, uncomment the below line
 # # rows = int(input('Enter the number of rows'))

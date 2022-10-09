ba8cea54419004ce5b5b804527147f3d46a30330
ARIFUL ISLAM ARIF <ariful.mr.islam@gmail.com>
2022-10-09 20:58:53 +0600

    Python and OOP

---
+++ Module 3.5 Practice Day/problem13.py
@@ -0,0 +1,9 @@
+print("Here is a  scarf:")
+
+print('~#' * 10)
+print('#-' * 10)
+print('Here is a wave:')
+print('        /\ ')
+print('       /  \ ')
+print('      /____\ ')
+print('     /      \ ')

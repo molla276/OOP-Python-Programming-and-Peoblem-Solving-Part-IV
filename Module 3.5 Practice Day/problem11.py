508c408120ee38a798b38e399ab916f528ec082a
ARIFUL ISLAM ARIF <ariful.mr.islam@gmail.com>
2022-10-09 20:07:46 +0600

    Python and OOP

---
+++ Module 3.5 Practice Day/problem11.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+print('My favourite animal are sheep')
+print('o-###-')
+print('  | |  #')
+print('I love in Glasgow')
+print('   _|_   ')
+print("  |   | ")
+print("  |#  | ")
+print("  |   |___ ")
+print("  |  #|   |")
+print("  |   |   |")
+print("  |   |  #|")
+print(" _|___|_#_|_")
+
+
+
+Powered by 
+My favourite animal are sheep
+o-###-
+  | |  #
+I love in Glasgow
+   _|_   
+  |   | 
+  |#  | 
+  |   |___ 
+  |  #|   |
+  |   |   |
+  |   |  #|
+ _|___|_#_|_

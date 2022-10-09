b798a429e3e2f1be5ccbdd072f82c03e553fa19d
ARIFUL ISLAM ARIF <ariful.mr.islam@gmail.com>
2022-10-09 20:42:17 +0600

    Python and OOP

---
+++ Module 3.5 Practice Day/python12.py
@@ -0,0 +1,15 @@
+Challenge: your age in dog years
+Write a program to ask the user their age, and then tell them their age in dog years! You can calculate a person’s age in dog years by multiplying their age by 7.
+
+age = int(input("What is your age? "))
+
+print("If you were a dog, you'd be " + str(age * 7) + " !!")
+
+print("o____")
+print(" ||||")
+
+
+What is your age?  9
+If you were a dog, you'd be 63 !!
+o____
+ ||||

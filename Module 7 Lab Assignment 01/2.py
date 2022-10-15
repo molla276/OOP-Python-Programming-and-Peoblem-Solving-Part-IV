79efd21f9c3d3563ddec8ea868a589ff115d7a8e
ARIFUL ISLAM ARIF <ariful.mr.islam@gmail.com>
2022-10-16 05:54:57 +0600

    Update 2.py

--- Module 7 Lab Assignment 01/2.py
+++ Module 7 Lab Assignment 01/2.py
@@ -7,7 +7,7 @@ import requests
 import time
 
 def weather_data():
-    pi_address = 'https://api.openweathermap.org/data/2.5/weather?q=Dhaka&exclude=30&appid=de1c9a8d36c6691c6a882a7ee946dc4b&units=metric'
+    pi_address = 'https://api.openweathermap.org/data/2.5/weather?q=Dhaka&exclude=30&appid="collect api id from openweathermap"&units=metric'
     url = pi_address
     json_data = requests.get(url).json()
     print(json_data)

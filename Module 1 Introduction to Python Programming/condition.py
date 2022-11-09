77191beb2fd5173d46e67a12fb38a77fa195ca95
ARIFUL ISLAM ARIF <ariful.mr.islam@gmail.com>
2022-11-09 07:23:29 +0600

    Create condition.py

---
+++ Module 1 Introduction to Python Programming/condition.py
@@ -0,0 +1,6 @@
+saarc = ['Bangladesh', 'Pakistan', 'Butan', 'Nepal', 'Maldive', 'Afganistan', 'India', 'Srilank]
+country = input('Enter your country name: '}
+
+if country in saarc:
+   print(country,'is a name of saarc')
+print('Programme tarminated')
